a98d826978eaf4b6f10db241e3b97da854daf102
johnlemak <lemak91@mac.com>
2022-02-03 12:45:32 -0600

    commit tree d1349dd2cdbe4ccc249fa11cb4590df891d73656

---
+++ index.js
@@ -0,0 +1,17 @@
+// function divide(num1, num2){
+//     return num1 / num2;
+// }
+
+// divide(2000, 100)
+// console.log(divide(2000, 100))
+
+const divide = (num1, num2) => 2000 / 100;
+const square = x => x ** 2; 
+const add = (num1, num2) => num1 + num2;
+
+console.log(add(2,4))
+
+// const x=4;
+
+
+// console.log(square()) 
